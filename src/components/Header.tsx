60c433b5ea14c3cdf3625c36642b21453aebd266
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-10-18 16:16:16 -0300

    DashGo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return(
@@ -13,6 +13,19 @@ export function Header() {
         color: 'gray.400'
       }}/>
       <Icon as={RiSearchLine} fontSize="20"/>
+      </Flex>
+      <Flex align="center" ml="auto" >
+        <HStack spacing="8" mx="8" pr="8" py="1" color="gray.300" borderRightWidth={1} borderColor="gray.700">
+          <Icon as={RiNotificationLine} fontSize="20"/>
+          <Icon as={RiUserAddLine} fontSize="20"/>
+        </HStack>
+        <Flex align="center">
+        <Box mr="4" textAlign="right">
+          <Text>Paulo Henrique</Text>
+          <Text color="gray.300" fontSize="small">paulinho@gmail.com</Text>
+        </Box>
+        <Avatar size="md" name="Paulo Henrique" src="https://github.com/Dev-Paulo-Henrique.png"/> 
+        </Flex>
       </Flex>
     </Flex>
   );
