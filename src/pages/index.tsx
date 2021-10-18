8b02732fd91b9e1cc1ac287585d6e826c940f15d
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-10-18 15:07:07 -0300

    DashGo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Flex,  Button, Stack } from '@chakra-ui/react'
 import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
 <Flex w="100vw" h="100vh" align="center" justify="center">
 <Flex as="form" w="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDir="column">
