97aa1ebd863038bc2aa6977771ed67d788aa5499
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-11-18 16:24:55 -0300

    DashGo

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Heading, Divider, VStack, SimpleGrid, HStack, Button } from 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Input } from "../../components/Form/Input";
+import Link from 'next/link'
 
 export default function CreateUser(){
   return(
@@ -24,7 +25,9 @@ export default function CreateUser(){
       </VStack>
       <Flex mt="8" justify="flex-end">
         <HStack spacing="4">
-          <Button colorScheme="whiteAlpha">Cancelar</Button>
+        <Link href="/users" passHref>
+          <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+          </Link>
           <Button colorScheme="pink">Salvar</Button>
         </HStack>
       </Flex>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,7 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import Link from 'next/link'
 
 export default function UserList(){
   const isWideVersion = useBreakpointValue({
@@ -18,9 +19,11 @@ export default function UserList(){
       <Box flex="1"  borderRadius={8} bg="gray.800" p="8">
       <Flex mb="8" justify="space-between" align="center">
         <Heading size="lg" fontWeight="normal">Usuários</Heading>
+        <Link href="users/create" passHref>
         <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>}>
           Criar novo
         </Button>
+        </Link>
       </Flex>
       <Table colorScheme="whiteAlpha">
         <Thead>
