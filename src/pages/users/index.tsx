a4d9e3b5ba47c2fbe66b46535315d7a8725c7775
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2021-12-07 14:35:07 -0300

    DashGo

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -78,7 +78,11 @@ export default function UserList(){
           })}
         </Tbody>
       </Table>
-      <Pagination/>
+      <Pagination
+      totalCountOfRegisters={200}
+      currentPage={10}
+      onPageChange={() => {}}
+      />
         </>
       )}
       </Box>
