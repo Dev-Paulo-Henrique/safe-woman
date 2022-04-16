36d5cb173087c19e7297742befc571cee8dfc8ee
Paulo Henrique <jwcfree11@gmail.com>
2022-04-16 17:53:49 -0300

    Delete stripe-js.ts

--- src/services/stripe-js.ts
+++
@@ -1,7 +0,0 @@
-import { loadStripe } from '@stripe/stripe-js'
-
-export async function getStripeJs(){
-  const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
-
-  return stripeJs
-} 
