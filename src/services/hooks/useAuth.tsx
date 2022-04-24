a2d4a8ef9633a7d8fbe9b7a08acb43e5a142078d
Paulo Henrique <jwcfree11@gmail.com>
2022-04-24 11:46:28 -0300

    sw

--- src/services/hooks/useAuth.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-// import AuthContext from "../contexts/AuthContext";
-
-// export default function useAuth() {
-//   const value = useContext(AuthContext);
-//   return value;
-// }
