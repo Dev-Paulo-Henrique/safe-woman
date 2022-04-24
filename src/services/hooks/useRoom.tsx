0d31c8aefade0aab2f28b4917f622927f69a1d19
Paulo Henrique <jwcfree11@gmail.com>
2022-04-24 11:46:10 -0300

    sw

--- src/services/hooks/useRoom.tsx
+++
@@ -1,8 +0,0 @@
-import { database, ref, set, onValue } from "../firebase";
-
-export default function useRoom() {
-  const roomRef = ref(database, 'users/');
-onValue(roomRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data)})
-}
