03f9a8cb6d6cad5fc6d8b477cc75d826c7917760
Paulo Henrique <jwcfree11@gmail.com>
2022-05-03 10:49:13 -0300

    sw

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,12 +10,12 @@ export default class MyDocument extends Document {
         <link rel="shortcut icon" href="https://d1wqzb5bdbcre6.cloudfront.net/c129603a39847da8056dccc1141bc95ba5732cf9cee3325309fc8241facf0ec2/68747470733a2f2f66696c65732e7374726970652e636f6d2f66696c65732f4d44423859574e6a644638785332343064486448626e5634553046704e58686b66475a666447567a6446396f4d45747855454a49644859304e4778335757527861445255616e4231616d67303039565579354b4872" type="image/png" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
         <meta name="google-site-verification" content="-j0SimXvp1t5MdqjZIy-zXv4IZmedaAhZeJCtFkTVJE" />
-<!--         <script src="https://www.gstatic.com/assistant/interactivecanvas/api/interactive_canvas.min.js"></script> -->
         <title>Safe Woman</title>
         </Head>
         <body>
           <Main/>
             <NextScript/>
+          <script src="https://www.gstatic.com/assistant/interactivecanvas/api/interactive_canvas.min.js"></script>
         </body>
       </Html>
     );
