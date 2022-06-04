c75ec5412b0e629dae3436c4b1f20c5ca2f3a97f
Paulo Henrique <jwcfree11@gmail.com>
2022-06-03 21:30:43 -0300

    sw

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -5,43 +5,43 @@ export function AboutUs(){
         <>
         <Flex flex="1" justifyContent="space-between" flexWrap="wrap" margin="2rem">
             <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Geovana Evelyn</Text>
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99beb914e288c5a6d97f/download/Geovana.jpeg"/>
+            <Text fontSize="lg" mt="0.5rem">Geovana Silva</Text>
             <Text color="gray.300" fontSize="small">Análise</Text>
             </Flex>
             <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Paulo Santos</Text>
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99c1b3cae167b7430287/download/Paulo.jpg"/>
+            <Text fontSize="lg" mt="0.5rem">Paulo Santos</Text>
             <Text color="gray.300" fontSize="small">Desenvolvimento</Text>
             </Flex>
             <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Rayssa Vitória</Text>
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99bf1bbe03889a69f10f/download/Rayssa.jpeg"/>
+            <Text fontSize="lg" mt="0.5rem">Rayssa Silva</Text>
             <Text color="gray.300" fontSize="small">Design</Text>
             </Flex>
             <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Eduarda Maria</Text>
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99c63d885c76db91d799/download/Eduarda.jpeg"/>
+            <Text fontSize="lg" mt="0.5rem">Eduarda Silva</Text>
             <Text color="gray.300" fontSize="small">Gestão</Text>
             </Flex>
             <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Caillany Maria</Text>
-            <Text color="gray.300" fontSize="small">Gestão</Text>
-            </Flex>
-            <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Daniel Emerson</Text>
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99c0028fa042d6ea7aa0/download/Daniel.jpeg"/>
+            <Text fontSize="lg" mt="0.5rem">Daniel Santos</Text>
             <Text color="gray.300" fontSize="small">Marketing</Text>
             </Flex>
             <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Erica Karolainy</Text>
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99bb367e7b76cf442ce1/download/Erica.jpeg"/>
+            <Text fontSize="lg" mt="0.5rem">Erica Santiago</Text>
             <Text color="gray.300" fontSize="small">Negócios</Text>
             </Flex>
             <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
-            <Avatar/>
-            <Text>Aline Lins</Text>
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99c34265ae6247687fb9/download/Caillany.jpeg"/>
+            <Text fontSize="lg" mt="0.5rem">Caillany Mariano</Text>
+            <Text color="gray.300" fontSize="small">Suporte</Text>
+            </Flex>
+            <Flex flexBasis="50%" mb="2rem" flexDirection="column" alignItems="center" justifyContent="center">
+            <Avatar size="2xl" src="https://trello.com/1/cards/629a99a52a684d06bb95777e/attachments/629a99c5bbe8bd019c30ce54/download/Aline.jpeg"/>
+            <Text fontSize="lg" mt="0.5rem">Aline Lins</Text>
             <Text color="gray.300" fontSize="small">Teste</Text>
             </Flex>
         </Flex>
