7d28d1689ea68113d6dfec7176d2dcbb8bea8cb7
Paulo Henrique <jwcfree11@gmail.com>
2022-07-19 10:27:43 -0300

    sw

--- src/pages/avaliar/[id].tsx
+++ src/pages/avaliar/[id].tsx
@@ -18,6 +18,7 @@ import {
   UnorderedList,
   } from "@chakra-ui/react";
   import Head from 'next/head'
+  import toast from 'react-hot-toast';
 //   import '../../styles/star.css'
 
 import {api} from '../../services/integration';
@@ -28,6 +29,15 @@ export default function Avaliar() {
     md: true
   })
 
+  const [ loading, setLoading ] = useState(false)
+
+  function Loading(){
+    setLoading(true)
+    new Promise(() => setTimeout(() => {setLoading(false), toast.success('Obrigado por sua avaliação!', {
+      duration: 4000,
+    });}, 2000))
+  }
+
   return (
     <Flex h="100vh" alignItems="center" justifyContent="center">
         <Head>
@@ -35,7 +45,7 @@ export default function Avaliar() {
     </Head>
       <Flex justifyContent="center" alignItems="center" flexDir="column" bg="#1F2029" borderRadius="0.375rem" p="30px 20px">
         <Text fontSize="20px" fontWeight="bold" textAlign="center" display="block" color="#fff">Avaliação</Text>
-<div className="estrelas">
+<Box className="estrelas">
   {/* <input type="radio" id="cm_star-empty" name="fb" value="" checked/> */}
   <label htmlFor="cm_star-1"><i className="fa"></i></label>
   <input type="radio" id="cm_star-1" name="fb" value="1"/>
@@ -47,7 +57,23 @@ export default function Avaliar() {
   <input type="radio" id="cm_star-4" name="fb" value="4"/>
   <label htmlFor="cm_star-5"><i className="fa"></i></label>
   <input type="radio" id="cm_star-5" name="fb" value="5"/>
-</div>
+</Box>
+<Button
+            // as={'a'}
+              // display={{ base: 'none', md: 'inline-flex' }}
+              mt={2}
+              fontSize={'sm'}
+              fontWeight={600}
+              color={'white'}
+              bg={'pink.500'}
+              isLoading={loading}
+              // href={'/login'}
+              onClick={Loading}
+              _hover={{
+                bg: 'pink.400',
+              }}>
+              Enviar
+            </Button>
       </Flex>
     </Flex>
   );
